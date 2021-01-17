193889537af0ad810fdd1d2343326e56a52ccb9c
Alexandra Justine Tuliao <avtuliao@up.edu.ph>
2021-01-17 19:43:36 +0800

    Added router-view to Login

--- lis162/src/App.vue
+++ lis162/src/App.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 v-app(style="background: #ffffff")
-  v-content
   router-view
 </template>
 
--- lis162/src/views/Admin.vue
+++ lis162/src/views/Admin.vue
@@ -1,29 +1,26 @@
 <template lang="pug">
-    v-container
-      v-row.justify-content-center.align-center
-        v-col(cols="10").mt-6
-          v-row 
-            v-col(cols="10")
-              v-text-field(
-                placeholder="Search Name"
-                solo
-                dense
-                prepend-inner-icon="mdi-magnify"
-                clearable
-              )
-            v-col(cols="2")
-              v-btn(label="Search")
-        v-col(cols="2")
-          v-btn(label="Add Member").mr-5
-          v-btn(label="Add Skills")
+v-container
+  v-row.justify-content-center.align-center
+    v-col.mt-6(cols="10")
       v-row 
-        v-simple-table
+        v-col(cols="10")
+          v-text-field(
+            placeholder="Search Name",
+            solo,
+            dense,
+            prepend-inner-icon="mdi-magnify",
+            clearable
+          )
+        v-col(cols="2")
+          v-btn(label="Search")
+    v-col(cols="2")
+      v-btn.mr-5(label="Add Member")
+      v-btn(label="Add Skills")
+  v-row 
+    v-simple-table
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style scoped>
-
 </style>
