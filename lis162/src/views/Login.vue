917d43f9ff54d6cb3fce3c07d335b1c4316f2b48
Alexandra Justine <59907792+ajvtuliao@users.noreply.github.com>
2021-01-17 19:59:40 +0800

    Update Login.vue

--- lis162/src/views/Login.vue
+++ lis162/src/views/Login.vue
@@ -21,106 +21,57 @@ v-container
             span.ml-n3.font-weight-black.display-2.join.mt-3 Join
             span.font-weight-black.display-3.intech.ml-3.mt-3 UP INTECH!
     v-col(cols="7")
-      v-card.mt-5.mb-10.align-center.mx-auto(
-        outlined,
-        elevation="2",
-        width="600px",
-        color="rgba(225, 244, 247, 0.1"
-      )
+      v-card.mt-5.mb-10.align-center.mx-auto(outlined elevation="2" width="600px" color="rgba(225, 244, 247, 0.1   ")
         v-row
-          v-col 
-            v-card-title
-              span.ml-n3.font-weight-black.display-2.learn.mt-3 Learn
-              h2.font-weight-light.program.ml-3.mt-3 programming...
-            v-card-title.mt-n7
-              span.ml-n3.font-weight-black.display-2.earn.mt-3 Earn 
-              h2.font-weight-light.money.ml-3.mt-3 money...
-            v-card-title.mt-n7
-              span.ml-n3.font-weight-black.display-2.enhance.mt-3 Enhance
-              h2.font-weight-light.skills.ml-3.mt-3 your coding skills...
-            v-card-title.mt-n7
-              span.ml-n3.font-weight-black.display-2.join.mt-3 Join
-              span.font-weight-black.display-3.intech.ml-3.mt-3 UP INTECH!
-      v-col(cols="7")
-        v-card.mt-5.mb-10.align-center.mx-auto(outlined elevation="2" width="600px" color="rgba(225, 244, 247, 0.1    ")
-          v-row
-            v-col(cols="4")
-            v-col(cols="5")
-              v-card-title.justify-content-center.ml-n3.mt-3
-                span.mr-3.font-weight-black.display-2.sign SIGN 
-                span.font-weight-light.display-2.up UP
-            v-col(cols="3")
-          v-row
-            v-col
-              v-row
-                v-col
-                v-col(cols="10")
-                  v-form(v-model="valid")
-                    v-text-field.mt-5(
-                      outlined label="Name" 
-                      placeholder="Last Name, First Name, M.I" 
-                      input="text"
-                      :rules="[rules.required]"
-                      )
-                v-col
-              v-row
-                v-col
-                v-col(cols="10")
-                  v-form(v-model="valid")
-                    v-text-field.mt-5(outlined label="Email" input="text" :rules="[rules.required]")
-                v-col
-              v-row 
-                v-col
-                v-col(cols="10")
-                  v-form(v-model="valid")
-                  v-text-field.mt-1.input-group--focused(
-                    v-model="password1"
-                    outlined 
-                    label="Password" 
-                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          v-col(cols="4")
+          v-col(cols="5")
+            v-card-title.justify-content-center.ml-n3.mt-3
+              span.mr-3.font-weight-black.display-2.sign SIGN 
+              span.font-weight-light.display-2.up UP
+          v-col(cols="3")
+        v-row
+          v-col
+            v-row
+              v-col
+              v-col(cols="10")
+                v-form(v-model="valid")
+                  v-text-field.mt-5(
+                    outlined label="Name" 
+                    placeholder="Last Name, First Name, M.I" 
+                    input="text"
                     :rules="[rules.required]"
-                    :type="show ? 'text' : 'password1'"
-                    @click:append="show = !show"
                     )
-                v-col
-              v-row
-                v-col 
-                v-col(cols="10")
-                  v-form(v-model="valid")
-                  v-text-field.mt-1.input-group--focused(
-                    v-model="password"
-                    outlined 
-                    label="Confirm Password" 
-                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                    :rules="[rules.required]"
-                  )
+              v-col
+            v-row
+              v-col
+              v-col(cols="10")
+                v-form(v-model="valid")
+                  v-text-field.mt-5(outlined label="Email" input="text" :rules="[rules.required]")
               v-col
             v-row 
               v-col
               v-col(cols="10")
                 v-form(v-model="valid")
                 v-text-field.mt-1.input-group--focused(
-                  v-model="password",
-                  outlined,
-                  label="Password",
-                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'",
-                  :rules="[rules.required]",
-                  :type="show ? 'text' : 'password'",
+                  v-model="password1"
+                  outlined 
+                  label="Password" 
+                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required]"
+                  :type="show ? 'text' : 'password1'"
                   @click:append="show = !show"
-                )
+                  )
               v-col
             v-row
               v-col 
               v-col(cols="10")
                 v-form(v-model="valid")
                 v-text-field.mt-1.input-group--focused(
-                  v-model="password",
-                  outlined,
-                  label="Confirm Password",
-                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'",
-                  :rules="[rules.required]",
-                  :type="show ? 'text' : 'password'",
-                  @click:append="show1 = !show1"
+                  v-model="password"
+                  outlined 
+                  label="Confirm Password" 
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required]"
                 )
               v-col
             v-row 
