2358ffbd98b608caaf555491cfec442321e9cc6a
Alexandra Justine Tuliao <avtuliao@up.edu.ph>
2021-01-19 22:49:42 +0800

    Missing skills, implemented search and removed search button

--- lis162/src/views/Admin.vue
+++ lis162/src/views/Admin.vue
@@ -3,7 +3,7 @@
     router-view
     v-container
       v-row.justify-content-center.align-center.mt-3.ml-n10.mr-n16
-        v-col(cols="7").mr-n1.mt-1
+        v-col(cols="8").mr-n1.mt-1
           v-text-field(
             placeholder="Search Name",
             solo,
@@ -12,19 +12,19 @@
             clearable
             v-model="search"
           )
-        v-col(cols="1").mr-n3
-          v-btn( dark color="#FE8848").mt-n6
-            span Search
+        //v-col(cols="1").mr-n3
+        //  v-btn( dark color="#FE8848").mt-n6
+        //    span Search
         v-col.mr-n6
           //- Add Member Button
         v-col(cols="3").mt-n6
           v-row.mr-n2
             v-col(cols="6").ml-n8
-              v-btn(@click="dialog = !dialog" dark color="rgba(117, 204, 112, 0.9)")
+              v-btn(@click="dialog.show = !dialog.show" dark color="rgba(117, 204, 112, 0.9)")
                 v-icon.ml-n2.mr-2 mdi-account-plus
                 span Add Member
               // dialog
-              v-dialog(v-model="dialog" width="800")
+              v-dialog(v-model="dialog.show" width="800")
                 v-card.pa-3(width="800px")
                   v-row
                     v-col.mt-n3.mb-3
@@ -33,89 +33,87 @@
                     v-col(cols="3")
                       v-card-subtitle.font-weight-bold Name
                     v-col(cols="9")
-                      v-text-field(outlined prepend-inner-icon="mdi-account" v-model="name" label="Member Name")
+                      v-text-field(outlined prepend-inner-icon="mdi-account" v-model="dialog.name" label="Member Name")
                   v-row.mx-2
                     v-col(cols="3")
                       v-card-subtitle.font-weight-bold Membership Date
                     v-col(cols="9")
                       v-dialog(
-                        ref="dialog"
-                        :return-value.sync="date"
+                        ref="dialog_2"
+                        :return-value.sync="dialog.date"
                         width="300px"
-                        v-model="modal"
+                        v-model="dialog.modal"
                       )
                         template(v-slot:activator="{on, attrs}")
                           v-text-field(
                             outlined
-                            v-model="date"
+                            v-model="dialog.date"
                             label="Membership Date"
                             prepend-icon="mdi-calendar"
                             v-bind="attrs"
                             v-on="on"
                           )
                         v-date-picker(
-                          v-model="date"
-                          type="month"
+                          v-model="dialog.date"
                           scrollable
                           color="#6adcc9"
                           header-color="#2bb69f"
                         )
                           v-spacer
-                          v-btn(dark color="#ff4040" @click="modal = false").mr-3.mb-2
+                          v-btn(dark color="#ff4040" @click="dialog.modal = false").mr-3.mb-2
                             span Cancel
-                          v-btn(dark color="#4747EB" @click="$refs.dialog.save(date)").mb-2
+                          v-btn(dark color="#4747EB" @click="$refs.dialog_2.save(dialog.date)").mb-2
                             span Save
                   v-row.mx-2
                     v-col(cols="3")
                       v-card-subtitle.font-weight-bold Membership Status
                     v-col(cols="9")
-                      v-select(v-model="status" outlined label="Status" :items="memstat")
+                      v-select(v-model="dialog.status" outlined label="Status" :items="memstat")
                   v-row.mx-2
                     v-col(cols="3")
                       v-card-subtitle.font-weight-bold Skills
                     v-col(cols="9")
-                      v-select(v-model="skill" outlined label="Skills" multiple)
+                      v-select(v-model="dialog.skills" outlined label="Skills" multiple)
                   v-row.mx-2
                     v-col(cols="3")
                       v-card-subtitle.font-weight-bold Current Project
                     v-col(cols="9")
                       v-row
                         v-col(cols="7")
-                          v-text-field(v-model="project" outlined prepend-inner-icon="mdi-clipboard-file" label="Project Name")
+                          v-text-field(v-model="dialog.current" outlined prepend-inner-icon="mdi-clipboard-file" label="Project Name")
                         v-col(cols="5")
                           v-dialog(
-                            ref="dialog"
-                            :return-value.sync="projdate"
+                            ref="dialog_3"
+                            :return-value.sync="dialog.start"
                             width="300px"
-                            v-model="modal1"
+                            v-model="dialog.modal1"
                           )
                             template(v-slot:activator="{on, attrs}")
                               v-text-field(
                                 outlined
-                                v-model="projdate"
+                                v-model="dialog.start"
                                 label="Starting Date"
                                 prepend-icon="mdi-calendar"
                                 v-bind="attrs"
                                 v-on="on"
                               )
                             v-date-picker(
-                              v-model="projdate"
-                              type="month"
+                              v-model="dialog.start"
                               scrollable
                               color="#6adcc9"
                               header-color="#2bb69f"
                             )
                               v-spacer
-                              v-btn(dark color="#ff4040" @click="modal = false").mr-3.mb-2
+                              v-btn(dark color="#ff4040" @click="dialog.modal1 = false").mr-3.mb-2
                                 span Cancel
-                              v-btn(dark color="#4747EB" @click="$refs.dialog.save(date)").mb-2
+                              v-btn(dark color="#4747EB" @click="$refs.dialog_3.save(dialog.start)").mb-2
                                 span Save  
                   v-card-actions
                     v-spacer
                     v-btn.mr-2(
                       dark,
                       color="#ff4040",
-                      @click="dialog = false"
+                      @click="dialog.show = false"
                     )
                       v-icon.mr-1 mdi-close
                       span.mr-2 Close
@@ -123,6 +121,7 @@
                       dark,
                       color="#4747EB",
                       :loading="loading",
+                      @click="addmember"
                     )
                       v-icon mdi-account-plus
                       v-spacer
@@ -130,12 +129,12 @@
                       v-spacer
             //-  Add Skill Button
             v-col(cols="6").ml-5
-              v-btn(dark color="#fad132" @click="dialog1 = !dialog1")
+              v-btn(dark color="#fad132" @click="dialog1.show = !dialog1.show")
                 v-icon.ml-n2.mr-2 mdi-plus-box
                 span Add Skill
               //dialog1
               v-dialog(
-                v-model="dialog1"
+                v-model="dialog1.show"
                 width="600px"
               )
                 v-card(width="600px").pa-3
@@ -148,21 +147,21 @@
                     v-col(cols="9")
                       v-text-field(
                         outlined
-                        v-model="skill"
+                        v-model="dialog1.skill"
                         label="Skill Name"
                       )
                   v-row.mx-2
                     v-col(cols="3").mt-10
                       v-card-subtitle.font-weight-bold Description
                     v-col(cols="9")
-                      v-textarea(outlined v-model="skilldesc" label="Skill Description" auto-grow)
+                      v-textarea(outlined v-model="dialog1.skilldesc" label="Skill Description" auto-grow)
 
                   v-card-actions
                     v-spacer
                     v-btn.mr-2(
                       dark,
                       color="#ff4040",
-                      @click="dialog1 = false"
+                      @click="dialog1.show = false"
                     )
                       v-icon.mr-1 mdi-close
                       span.mr-2 Close
@@ -170,6 +169,7 @@
                       dark,
                       color="#4747EB",
                       :loading1="loading1",
+                      @click="addskill"
                     )
                       v-spacer
                       span Add Skill
@@ -216,19 +216,19 @@
                       v-col(cols="8")
                         v-row
                           v-col(cols="7")
-                            //- Project Name
+                            v-card-text {{ dialog3.current }}
                           v-col(cols="5")
-                            //- Project Starting Daate
+                            v-card-text {{ dialog3.start }}
                     v-card-actions
                       v-spacer
                       v-btn.mr-2(
                         dark,
                         color="#ff4040",
-                        @click="dialog3 = false"
+                        @click="dialog3.show = false"
                       )
                         v-icon.mr-1 mdi-close
                         span.mr-2 Close
-                tr(v-for="member in members" :key="member.id" )
+                tr(v-for="member in membersFilter" :key="member.id" )
                   //- Member Name
                   td.text-center.font-weight-bold(@click="get_member_details_3(member.id)") {{ member.name }}
                   //- Member Status
@@ -261,28 +261,27 @@
                               ref="dialog"
                               :return-value.sync="dialog2.date"
                               width="300px"
-                              v-model="modal"
+                              v-model="dialog2.modal"
                             )
-                              template(v-slot:activator="{on, attrs}")
+                              template(v-slot:activator="{on}")
                                 v-text-field(
+                                  readonly
                                   outlined
                                   v-model="dialog2.date"
                                   label="Membership Date"
                                   prepend-icon="mdi-calendar"
-                                  v-bind="attrs"
                                   v-on="on"
                                 )
                               v-date-picker(
-                                v-model="date"
-                                type="month"
+                                v-model="dialog2.date"
                                 scrollable
                                 color="#6adcc9"
                                 header-color="#2bb69f"
                               )
                                 v-spacer
-                                v-btn(dark color="#ff4040" @click="modal = false").mr-3.mb-2
+                                v-btn(dark color="#ff4040" @click="dialog2.modal = false").mr-3.mb-2
                                   span Cancel
-                                v-btn(dark color="#4747EB" @click="$refs.dialog.save(dialog2.date)").mb-2
+                                v-btn(dark color="#4747EB" @click="$refs.dialog[member.id-1].save(dialog2.date)").mb-2
                                   span Save
                         v-row.mx-2
                           v-col(cols="3")
@@ -300,39 +299,40 @@
                           v-col(cols="9")
                             v-row
                               v-col(cols="7")
-                                v-text-field(v-model="project" outlined prepend-inner-icon="mdi-clipboard-file"       label="Project Name")
+                                v-text-field(v-model="dialog2.current" outlined prepend-inner-icon="mdi-clipboard-file" label="Project Name")
                               v-col(cols="5")
                                 v-dialog(
                                   width="300px"
-                                  v-model="modal1"
+                                  v-model="dialog2.modal1"
+                                  :return-value.sync="dialog2.start"
+                                  ref="dialog_1"
                                 )
                                   template(v-slot:activator="{on, attrs}")
                                     v-text-field(
                                       outlined
-                                      v-model="projdate"
+                                      v-model="dialog2.start"
                                       label="Starting Date"
                                       prepend-icon="mdi-calendar"
                                       v-bind="attrs"
                                       v-on="on"
                                     )
                                   v-date-picker(
-                                    v-model="projdate"
-                                    type="month"
+                                    v-model="dialog2.start"
                                     scrollable
                                     color="#6adcc9"
                                     header-color="#2bb69f"
                                   )
                                     v-spacer
-                                    v-btn(dark color="#ff4040" @click="modal1 = false").mr-3.mb-2
+                                    v-btn(dark color="#ff4040" @click="dialog2.modal1 = false").mr-3.mb-2
                                       span Cancel
-                                    v-btn(dark color="#4747EB").mb-2
+                                    v-btn(dark color="#4747EB" @click="$refs.dialog_1[member.id-1].save(dialog2.start)").mb-2
                                       span Save  
                         v-card-actions
                           v-spacer
                           v-btn.mr-2(
                             dark,
                             color="#ff4040",
-                            @click="dialog2 = false"
+                            @click="dialog2.show = false"
                           )
                             v-icon.mr-1 mdi-close
                             span.mr-2 Cancel
@@ -340,6 +340,7 @@
                             dark,
                             color="#4747EB",
                             :loading2="loading2",
+                            @click="editmember(member.id)"
                           )
                             v-icon.mr-2 mdi-clipboard-edit
                             v-spacer
@@ -347,7 +348,7 @@
                             v-spacer
                   //-  Remove Button
                   td.text-center
-                    v-btn(color="#ff4040" dark small)
+                    v-btn(color="#ff4040" dark small @click="remove(member.id)")
                       v-icon mdi-trash-can 
                       span Remove Member
 
@@ -359,8 +360,6 @@ export default {
   data() {
     return {
       search: '',
-      dialog: false,
-      dialog1: false,
       date: new Date().toISOString().substr(0, 7),
       modal: false,
       modal1: false,
@@ -373,8 +372,6 @@ export default {
       project: '',
       projdate: new Date().toISOString().substr(0, 7),
       skills: [],
-      skill: '',
-      skilldesc: '',
       memstat: [
         "Active",
         "Inactive",
@@ -383,13 +380,32 @@ export default {
         "Applicant",
       ],
       members: [],
-      dialog2: {
+      dialog: {
+        show:false,
+        name: "",
+        status: "",
+        date: "",
+        skills: "",
+        current: "",
+        start: "",
+        modal: false,
+        modal1: false,
+      },
+      dialog1: {
+        show:false,
+        skill: '',
+        skilldesc: '',
+      },
+      dialog2: { // edit member
         show: false,
         name: "",
         status: "",
         date: "",
         skills: "",
-        current: ""
+        current: "",
+        modal: false,
+        modal1: false,
+        start: ""
       },
       dialog3: {
         show: false,
@@ -397,7 +413,8 @@ export default {
         status: "",
         date: "",
         skills: "",
-        current: ""
+        current: "",
+        start: ""
       },
     };
   },
@@ -425,6 +442,8 @@ export default {
         this.dialog2.date = member.date;
         this.dialog2.skills = member.skills;
         this.dialog2.current = member.current;
+        this.dialog2.start = member.start;
+
       });
       this.dialog2.show = !this.dialog2.show;
     },
@@ -432,13 +451,59 @@ export default {
       // console.log(id);
       axios.get('http://localhost:8000/api/members/member/'+id).then(response => {
         let member = response.data[0];
-        this.dialog3_name = member.name;
-        this.dialog3_status = member.status;
-        this.dialog3_date = member.date;
-        this.dialog3_skills = member.skills;
-        this.dialog3_current = member.current;
+        this.dialog3.name = member.name;
+        this.dialog3.status = member.status;
+        this.dialog3.date = member.date;
+        this.dialog3.skills = member.skills;
+        this.dialog3.current = member.project;
+        this.dialog3.start = member.start;
+        console.log(member);
       });
-      this.dialog3 = !this.dialog3;
+      this.dialog3.show = !this.dialog3.show;
+    },
+    addskill() {
+      axios.post('http://localhost:8000/api/skills', {
+        skill: this.dialog1.skill,
+        skilldesc: this.dialog1.skilldesc
+      }).then(response => {
+        console.log(response)
+        this.dialog1.show = !this.dialog1.show;
+      })
+    },
+    editmember(id) {
+      axios.put('http://localhost:8000/api/members/member/'+id+'/edit', {
+        name: this.dialog2.name,
+        status: this.dialog2.status,
+        date: this.dialog2.date,
+        current: this.dialog2.current,
+        start: this.dialog2.start,
+      }).then(response => {
+        console.log(response)
+        this.dialog2.show=false;
+      })
+    },
+    addmember() {
+      axios.post('http://localhost:8000/api/members/member/', {
+        name: this.dialog.name,
+        status: this.dialog.status,
+        date: this.dialog.date,
+        skills: this.dialog.skills,
+        current: this.dialog.current,
+        start: this.dialog.start,
+      }).then(response => {
+        console.log(response)
+        this.dialog.show = false;
+        axios.get("http://localhost:8000/api/members").then(response => {
+          this.members = response.data
+        })
+      })
+    },
+    remove(id) {
+      axios.post('http://localhost:8000/api/members/member/'+id+'/delete').then(() => {
+        axios.get("http://localhost:8000/api/members").then(response => {
+          this.members = response.data
+        })
+      })
     }
   },
   watch: {
@@ -454,6 +519,17 @@ export default {
       this.members = response.data
     })
   },
+  computed: {
+    membersFilter: function() {
+      if (this.search !== '') {
+        return this.members.filter(x => {
+          return x.name.toLowerCase().includes(this.search.toLowerCase());
+        })
+      } else {
+        return this.members
+      }
+    }
+  }
 };
 </script>
 <style scoped>
